9be54941ee44d2dd16732e17b1d2f984d12fa1c4
Sharayu Nashikkar <133528701+SharayuNashikkar@users.noreply.github.com>
2026-01-12 20:53:42 +0530

    Added javascript code
    Restored console logs and prompt functionality for user interaction.

---
+++ practice.js
@@ -0,0 +1,33 @@
+// console.log("Hello, World!");
+
+// // Additional functionality can be added here
+// function greet(name) {
+//     return `Hello, ${name}!`;
+// }
+// console.log(greet("User"));
+// let firstName = "John";
+// console.log(`Welcome, ${firstName}!`);
+
+// let lastName = "Doe";
+// console.log(`Your last name is ${lastName}.`);
+// let fullName = `${firstName} ${lastName}`;
+// console.log(`Full Name: ${fullName}`);
+
+// let mixedData = `Name: ${firstName}, Age: ${30}, City: ${"New York"}`;
+// console.log(mixedData);
+
+
+// let abc = "hello";
+// let xyz = "BYE";
+// console.log(abc.length);
+// console.log(xyz.length);
+// console.log(abc[0]);
+// console.log(abc.toUpperCase());
+// console.log(xyz.toLowerCase());
+// console.log(abc.concat(" WORLD"));
+// console.log(xyz.concat(" EVERYONE"));
+
+// console.log(window);
+console.log(document);
+console.log(window.confirm("Are you sure you want to proceed?"));
+window.prompt("Please enter your name:");
