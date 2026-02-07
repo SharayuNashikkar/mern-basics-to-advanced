67d9ba80673beafebfa3d318f5f37939e4a4a2b6
SharayuNashikkar <sharayunashikkar@gmail.com>
2026-02-07 17:48:20 +0530

    change in index.html and script.js

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-function toggleText() {
-    const textElement = document.getElementById('text');
-    const button = document.getElementById('toggleBtn');
-    
-    if (button.textContent === 'Show text') {
-        // Show the paragraph
-        textElement.style.display = 'block';
-        button.textContent = 'Hide text';
-    } else {
-        // Hide the paragraph
-        textElement.style.display = 'none';
-        button.textContent = 'Show text';
-    }
-}
-
 // Add event listener when the page loads
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.getElementById('toggleBtn');
